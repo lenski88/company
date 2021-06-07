624e8a43b04327419df990d7dfda4f810e81533c
lenski <lenski88@icloud.com>
2021-06-08 00:17:25 +0300

    ...

---
+++ src/components/Company.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Company extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Company</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Company;
